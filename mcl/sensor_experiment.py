97a20ca24d0118fd4dc4b5e14ceaf032885c2587
imamura-slab <atuuyarumami0704@yahoo.co.jp>
2020-03-14 00:16:41 +0900

    5章まで

---
+++ mcl/sensor_experiment.py
@@ -0,0 +1,40 @@
+#!/opt/anaconda3/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('../scripts/')
+from robot import *
+import pandas as pd
+
+        
+
+def main():
+
+    m = Map()
+    m.append_landmark(Landmark(1,0))
+
+    distance  = []
+    direction = []
+    
+    for i in range(1000):
+        c = Camera(m)
+        d = c.data(np.array([0.0,0.0,0.0]).T)
+        if len(d) > 0:
+            distance.append(d[0][0][0])
+            direction.append(d[0][0][1])
+            
+    
+    df = pd.DataFrame()
+    df["distance"]  = distance
+    df["direction"] = direction
+    #print(df)
+    print(df.std())
+    print(df.mean())
+
+    
+if __name__ == '__main__':
+    main()
+
+
+
+    
